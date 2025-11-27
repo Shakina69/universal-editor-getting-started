83403507619bfc67da834a364e95a54276a658d2
Shakina69 <shakinakhan9667@gmail.com>
2025-11-27 21:29:12 +0530

    fix

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,9 +1,5 @@
-
-
-
 export default function decorate(block) {
- block.classList.add("test-class");
- const el = block.children[0].textContent.trim() + " test";
- console.log(el);
- block.children[0].innerText= el;
+  block.classList.add('test-class');
+  const el = `${block.children[0].textContent.trim()} test`;
+  block.children[0].innerText = el;
 }
