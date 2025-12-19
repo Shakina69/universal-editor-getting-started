99d66c787b08431c4c0fd4005f92efbf19a09e70
Shakina69 <shakinakhan9667@gmail.com>
2025-12-19 22:22:11 +0530

    changed

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -1,12 +1,7 @@
 export default function decorate(block) {
-  // Get color value from authoring
   const colorClass = block.children[1]?.textContent?.trim();
-
-  // Apply color class to text container
   if (colorClass) {
     block.children[0].classList.add(colorClass);
   }
-
-  // Remove authoring-only color field
   block.children[1].remove();
 }
